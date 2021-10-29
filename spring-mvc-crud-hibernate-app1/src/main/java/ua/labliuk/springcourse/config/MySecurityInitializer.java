16a90e0a97eccee08904daed00b0877ee5394f9b
Vladimir Labliuk <50242030+n1ceFella@users.noreply.github.com>
2021-10-28 23:35:09 -0400

    v.0.1.1

---
+++ spring-mvc-crud-hibernate-app1/src/main/java/ua/labliuk/springcourse/config/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package ua.labliuk.springcourse.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
